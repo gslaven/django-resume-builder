2444ca33d433eb49cac7c718d0174986ed6b4dac
Greg <32984362+gslaven@users.noreply.github.com>
2018-05-15 15:38:11 -0400

    Started Building Models - Reference picture on phone of chalkboard DB design

--- urls.py
+++ urls.py
@@ -2,18 +2,18 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 
-from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = [
-    path(r'^admin/', 
-    include(admin.site.urls)), 
-    path(r'^(?P<pk>\d+)/$', 
-    'resume_builder.views.resume')]
+    path(r'^admin/',
+    include(admin.site.urls)),
+    path(r'^(?P<pk>\d+)/$',
+    'resume_builder.views.resume'),
+    ]
 
 if settings.DEBUG:
     urlpatterns += [
-        path(r'^static/(?P<path>.*)$', 
-        'django.views.static.serve', 
+        path(r'^static/(?P<path>.*)$',
+        'django.views.static.serve',
         {'document_root': settings.MEDIA_ROOT})
-    ]
+        ]
